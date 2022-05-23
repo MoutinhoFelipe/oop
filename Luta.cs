3d8da495c78b345291b00dfa418df8e1bd46e596
MoutinhoFelipe <mouti.felipe@gmail.com>
2022-05-23 01:10:02 -0300

    Update 1

--- Luta.cs
+++ Luta.cs
@@ -5,15 +5,14 @@ class Jogo_de_Luta {
         Fighter f1 = new Fighter("Felipe", 20, 15, 15);
         Fighter f2 = new Fighter("Marcos", 15, 30, 5);
         Fight UFC1 = new Fight();
-        f1.presentation();
         UFC1.ft_fight(f1,f2);
     }
 }
 
 class Fighter {
-    public string name;
+    public string name {get;set;}
     public string type;
-    public int life;
+    public double life;
     public int strength;
     public int defense;
     public int quickness;
@@ -45,20 +44,55 @@ class Fighter {
         return agi;
     }
 
-    public string getName() {
-        return this.name;
-    }
-
     public void presentation() {
-        Console.WriteLine("Nome do Lutador: " + this.getName());
+        int i = 0;
+
+        Console.WriteLine("Nome do Lutador: " + this.name);
+        Console.WriteLine("Atributos do Lutador: ");
+
+        Console.Write("Força:     ");
+        while (i < (this.strength)){
+            Console.Write("|");
+            i++;
+        };
+        Console.WriteLine(" " + this.strength);
+        i = 0;
+
+        Console.Write("Defesa:    ");
+        while (i < (this.defense)){
+            Console.Write("|");
+            i++;
+        };
+        Console.WriteLine(" " + this.defense);
+        i = 0;
+
+        Console.Write("Agilidade: ");
+        while (i < (this.quickness)){
+            Console.Write("|");
+            i++;
+        };
+        Console.WriteLine(" " + this.quickness);
+        i = 0;
+        Console.WriteLine(" _______________________________ ");
     }
 }
 
 class Fight {
 
     public void ft_fight(Fighter f1, Fighter f2) {
-         Console.WriteLine("Luta iniciada!");
-         Console.WriteLine("Turno 1: " + ft_damage());
+        double dmg;
+        int j = 0;
+        f1.presentation();
+        f2.presentation();
+        Console.WriteLine("Luta iniciada!");
+
+        while ((f1.life > 0) && (f2.life > 0)) {
+            dmg = ft_damage(f1,f2);
+            Console.WriteLine("Turno 1: " + f1.name + " perdeu de life " + dmg);
+            f1.life -= dmg;
+            Console.WriteLine("Life atual: " + f1.life);
+            //j++;
+        }
     }
 
     public double ft_damage(Fighter f1, Fighter f2) {
@@ -68,6 +102,10 @@ class Fight {
         } else {
             dmg = f2.ft_attack() - f1.ft_defend();
         }
+        if (dmg < 0) {
+            dmg = 0;
+        }
+        dmg = 10;
         return dmg;
     }
 }
